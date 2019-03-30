92abea146da7cdba06195366e37819d188e87bb7
Mauricio Flores Hernández <mau1361317@gmail.com>
2019-03-29 20:02:45 -0600

    Fragments added to Home_inversionista

---
+++ app/src/main/java/com/devmaufh/inver_x/InverPerfil.java
@@ -0,0 +1,29 @@
+package com.devmaufh.inver_x;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class InverPerfil extends Fragment {
+
+
+    public InverPerfil() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inver_perfil, container, false);
+    }
+
+}
